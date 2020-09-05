e046ef667b815f14478e548ef8da455ddbef56ce
Eduardo Vilke <eduardo.vilke@unochapeco.edu.br>
2020-09-05 17:08:45 -0300

    Clean files

--- app/Controllers/Http/TipoController.js
+++ app/Controllers/Http/TipoController.js
@@ -3,37 +3,36 @@
 const Tipo = use("App/Models/TiposServico")
 
 class TipoController {
-    async index(){
-        return await Tipo.all()
-      }
-    
-      async show({ params }){
-        const tipo = await Tipo.findOrFail(params.id)
-        return tipo
-      }
-    
-      async store({ request }){
-        const data = request.all()
-        const tipo = await Tipo.create(data)
-        return tipo
-      }
-    
-      async update({params, request}){
-        const data = request.only([
-            "nome",
-            "descricao"
-        ])
-        const tipo = await Tipo.findOrFail(params.id)
-        tipo.merge(data)
-        await tipo.save()
-        return tipo
-      }
-    
-      async destroy({params}){
-        const tipo = await Tipo.findOrFail(params.id)
-        return await tipo.delete()
-    
-      }
+  async index(){
+      return await Tipo.all()
+    }
+  
+    async show({ params }){
+      const tipo = await Tipo.findOrFail(params.id)
+      return tipo
+    }
+  
+    async store({ request }){
+      const data = request.all()
+      const tipo = await Tipo.create(data)
+      return tipo
+    }
+  
+    async update({params, request}){
+      const data = request.only([
+          "nome",
+          "descricao"
+      ])
+      const tipo = await Tipo.findOrFail(params.id)
+      tipo.merge(data)
+      await tipo.save()
+      return tipo
+    }
+  
+    async destroy({params}){
+      const tipo = await Tipo.findOrFail(params.id)
+      return await tipo.delete()
+    }
 }
 
 module.exports = TipoController
--- app/Models/Situacao.js
+++ app/Models/Situacao.js
@@ -1,12 +1,11 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
 class Situacao extends Model {
-    servico (){
-        return this.belongsToMany('App/Models/Servico')
-    }
+  servico (){
+    return this.belongsToMany('App/Models/Servico')
+  }
 }
 
 module.exports = Situacao
