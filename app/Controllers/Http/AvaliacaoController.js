cce3053ee445b499cdb4c2820c2a4b179d37f838
Eduardo Vilke <eduardo.vilke@unochapeco.edu.br>
2020-09-05 17:07:01 -0300

    CRUD avaliacao

--- app/Controllers/Http/AvaliacaoController.js
+++ app/Controllers/Http/AvaliacaoController.js
@@ -1,6 +1,39 @@
 'use strict'
 
+const Avaliacao = use("App/Models/Avaliacao")
 class AvaliacaoController {
+  async index(){
+    return await Avaliacao.all()
+  }
+
+  async show({ params }){
+    const avaliacao = await Avaliacao.findOrFail(params.id)
+    return avaliacao
+  }
+
+  async store({ request }){
+    const data = request.all()
+    const avaliacao = await Avaliacao.create(data)
+    return avaliacao
+  }
+
+  async update({params, request}){
+    const data = request.only([
+      "descricao_avaliacao",
+      "nota_avaliacao",
+      "situacao_id",
+      "prestador_id"
+    ])
+    const avaliacao = await Avaliacao.findOrFail(params.id)
+    avaliacao.merge(data)
+    await avaliacao.save()
+    return avaliacao
+  }
+
+  async destroy({params}){
+    const avaliacao = await Avaliacao.findOrFail(params.id)
+    return await avaliacao.delete()
+  }
 }
 
 module.exports = AvaliacaoController
