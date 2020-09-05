ed5c59aa6892a82ea26e7068d12f2043ecc78078
Eduardo Vilke <eduardo.vilke@unochapeco.edu.br>
2020-09-05 17:07:56 -0300

    CRUD Servico

--- app/Controllers/Http/ServicoController.js
+++ app/Controllers/Http/ServicoController.js
@@ -1,6 +1,40 @@
 'use strict'
 
+const Servico = use("App/Models/Servico")
+
 class ServicoController {
+	async index(){
+    return await Servico.all()
+  }
+
+  async show({ params }){
+    const servico = await Servico.findOrFail(params.id)
+    return servico
+  }
+
+  async store({ request }){
+    const data = request.all()
+    const servico = await Servico.create(data)
+    return servico
+  }
+
+  async update({params, request}){
+    const data = request.only([
+      "descricao_avaliacao",
+      "nota_avaliacao",
+      "situacao_id",
+      "prestador_id"
+    ])
+    const servico = await Servico.findOrFail(params.id)
+    servico.merge(data)
+    await servico.save()
+    return servico
+  }
+
+  async destroy({params}){
+    const servico = await Servico.findOrFail(params.id)
+    return await servico.delete()
+  }
 }
 
 module.exports = ServicoController
